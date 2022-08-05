58a4a4202308d6d11f287b836a342068b19450be
Tim Benniks <tbenniks@gmail.com>
2022-08-05 08:56:26 +0200

    better intro copy

--- components/Intro.vue
+++ components/Intro.vue
@@ -1,24 +1,21 @@
 <template>
   <div class="max-w-[1440px] mx-auto px-8 lg:p-0">
     <p class="mb-12 text-xl leading-relaxed">
-      Tim Benniks is Principal Developer Advocate at
+      Tim Benniks is principal developer advocate at
       <a href="https://uniform.dev" rel="noopener" target="_blank">Uniform</a>,
       <a href="https://machalliance.org" rel="noopener" target="_blank"
-        >MACH Alliance</a
-      >
-      Tech Council Member,
+        >MACH Alliance Tech Council member</a
+      >,
       <a href="https://nuxtjs.org/teams" rel="noopener" target="_blank"
         >NuxtJS</a
       >
-      Ambassador,
+      ambassador,
       <a href="https://cloudinary.com/mde" rel="noopener" target="_blank"
-        >Cloudinary MDE</a
-      ><br />
-      and
+        >Cloudinary media developer expert (MDE)</a
+      >, and
       <a href="https://youtube.com/timbenniks" rel="noopener" target="_blank"
         >content creator</a
-      >
-      based in South-West France.
+      >. He lives in the Toulouse Metropolitan Area, France.
     </p>
   </div>
 </template>
